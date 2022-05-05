6f064649c0d0cf22e594e841067423002b5bf0f3
Rivgad <losvermo@gmail.com>
2022-05-05 16:08:57 +0300

    Добавил bootstrap и react-bootstrap

--- bars-group-delivery/client-app/src/index.js
+++ bars-group-delivery/client-app/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <>
     <h1>
       сервис доставки еды
     </h1>
+    <Button variant='primary'>Кнопка</Button>
+    <button className='btn btn-primary' type='button'></button>
     </>
   </React.StrictMode>
 );
