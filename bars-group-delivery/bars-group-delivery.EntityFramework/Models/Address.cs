203d048e917841aeda498bfc9e6490b081fb59b0
Rivgad <losvermo@gmail.com>
2022-05-18 16:20:23 +0300

    Change Address AccountId to required

--- bars-group-delivery/bars-group-delivery.EntityFramework/Models/Address.cs
+++ bars-group-delivery/bars-group-delivery.EntityFramework/Models/Address.cs
@@ -1,14 +1,44 @@
-﻿namespace bars_group_delivery.EntityFramework.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace bars_group_delivery.EntityFramework.Models
 {
     public class Address : Entity
     {
+        [Required]
+        [JsonPropertyOrder(0)]
+        [JsonPropertyName("accountId")]
+        public string? AccountId { get; set; }
+
+        [JsonPropertyOrder(1)]
+        [JsonPropertyName("city")]
         public string? City { get; set; }
+
+        [JsonPropertyOrder(2)]
+        [JsonPropertyName("street")]
         public string? Street { get; set; }
+
+        [JsonPropertyOrder(3)]
+        [JsonPropertyName("house")]
         public string? House { get; set; }
+
+        [JsonPropertyOrder(4)]
+        [JsonPropertyName("entrance")]
         public int? Entrance { get; set; }
+
+        [JsonPropertyOrder(5)]
+        [JsonPropertyName("intercom")]
         public string? InterCom { get; set; }
+
+        [JsonPropertyOrder(6)]
+        [JsonPropertyName("floot")]
         public int? Floor { get; set; }
+
+        [JsonPropertyOrder(7)]
+        [JsonPropertyName("flat")]
         public int? Flat { get; set; }
+
+        [JsonIgnore]
         public Account Account { get; set; }
     }
 }
