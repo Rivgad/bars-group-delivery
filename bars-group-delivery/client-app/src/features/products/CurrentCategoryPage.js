b4097d5c059b56cd7a4af41db67753d531b31754
Rivgad <losvermo@gmail.com>
2022-05-19 10:36:31 +0300

    Изменил интерфейс для CurrentCategoryPage Добавлен спинер загрузки и индикаторы ошибки

--- bars-group-delivery/client-app/src/features/products/CurrentCategoryPage.js
+++ bars-group-delivery/client-app/src/features/products/CurrentCategoryPage.js
@@ -7,6 +7,9 @@ import ProductsGrid from "./ProductGrid";
 import { Breadcrumb, BreadcrumbItem, Row } from "react-bootstrap";
 import { selectCategoryById } from "../categories/categoriesSlice";
 import { LinkContainer } from "react-router-bootstrap";
+import { RequestStatus } from "../../helpers";
+import ErrorIndicator from "../common/ErrorIndicator";
+import PageLoadingIndicator from "../common/PageLoadingIndicator";
 
 const CurrentCategoryPage = () => {
     let { id: categoryId } = useParams();
@@ -16,8 +19,27 @@ const CurrentCategoryPage = () => {
     }, [dispatch, categoryId]);
 
     const category = useSelector(state => selectCategoryById(state, categoryId));
+    const requestStatus = useSelector(state => state.products.status);
+    const isLoading = requestStatus === RequestStatus.Loading;
+    const isError = requestStatus === RequestStatus.Failed;
+
+    let renderedContent = '';
+    renderedContent =
+        (
+            <Row>
+                <ProductsGrid />
+            </Row>
+        );
+    if(!category){
+        renderedContent=<ErrorIndicator title={"Данной категории не существует"} message={"Перейдите в категории и выберите категорию снова."}/>
+    }
+    else if (isLoading) {
+        renderedContent = <PageLoadingIndicator/>
+    }
+    else if (isError) {
+        renderedContent = <ErrorIndicator message={"В данной категории нет товаров или произошла ошибка. Попробуйте снова."}/>
+    }
     return (
-        category &&
         <>
             <Row className='mb-3'>
                 <Breadcrumb >
@@ -27,13 +49,11 @@ const CurrentCategoryPage = () => {
                         </BreadcrumbItem>
                     </LinkContainer>
                     <BreadcrumbItem active>
-                        {category.title}
+                        {category?.title}
                     </BreadcrumbItem>
                 </Breadcrumb>
             </Row>
-            <Row>
-                <ProductsGrid />
-            </Row>
+            {renderedContent}
         </>
     );
 }
